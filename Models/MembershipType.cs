55a9274f4547d6e50889949a75b9fa4aeed56ea7
Delyanaa <delyanaaivanova@gmail.com>
2020-12-08 23:42:48 +0200

    Added Name prop + migration

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -1,8 +1,13 @@
-﻿namespace Vidley.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidley.Models
 {
     public class MembershipType
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DiscountRate { get; set; }
         public byte DurationInMonths { get; set; }
