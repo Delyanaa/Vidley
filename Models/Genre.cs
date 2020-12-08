134388f5d07653fdb144cff8a25de92c7d4edc32
Delyanaa <delyanaaivanova@gmail.com>
2020-12-09 00:49:32 +0200

    Added/Created Genre/s and added new props for Movies

---
+++ Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vidley.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(256)]
+        public String Name { get; set; }
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,19 @@ namespace Vidley.Models
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }    
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public int NumberInStock { get; set; }
+
+
+        public int GenreId { get; set; }
     }
 }
