e5bafbc567a430afdab68e4269dc55c0881e3e66
Delyanaa <delyanaaivanova@gmail.com>
2020-12-09 01:05:58 +0200

    Added gateway to the db in MoviesController

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -9,13 +9,7 @@ namespace Vidley.Controllers
 {
     public class MoviesController : Controller
     {
-        List<Movie> movieList = new List<Movie> {
-                new Movie{ Id = 1, Name = "Shrek!"},
-                new Movie{ Id = 2, Name = "Captain America"},
-                new Movie{ Id = 3, Name = "As above, so below"},
-                new Movie{ Id = 4, Name = "Marley and me"},
-                new Movie{ Id = 5, Name = "Gone"}
-            };
+        ApplicationdbContext _context
 
         [Route("movies")]
         public ActionResult Index()
