e07102c12c585b2d642c2eee5bf48d5a92e11818
Delyanaa <delyanaaivanova@gmail.com>
2020-12-11 22:07:45 +0200

    Refactoring Magic Strings

--- Models/DataAnnotations/Min18ForAMembership.cs
+++ Models/DataAnnotations/Min18ForAMembership.cs
@@ -12,7 +12,7 @@ namespace Vidley.Models.DataAnnotations
         {
             var customer = (Customer)validationContext.ObjectInstance;
 
-            if (customer.MembershipTypeId == 1)
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
                 return ValidationResult.Success;
 
             if (customer.Birthday == null)
