40cdf3d2def94cae02cf1069dc35934d6325e778
Delyanaa <delyanaaivanova@gmail.com>
2020-12-14 22:10:43 +0200

    Added DTOs for customer and membershiptype

---
+++ Dtos/CustomerDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidley.Models.DataAnnotations;
+
+namespace Vidley.Dtos
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [StringLength(225)]
+        [Required(ErrorMessage = "Please, enter your name!")]
+        public string Name { get; set; }
+
+        public MembershipTypeDTO MembershipTypeDTO { get; set; }
+
+        public bool IsSubscribeToNewsletter { get; set; }
+
+        [Min18ForAMembership]
+        public DateTime? Birthday { get; set; }
+
+        [Required(ErrorMessage = "Please, specify the type of membership.")]
+        public int MembershipTypeId { get; set; }
+    }
+}
---
+++ Dtos/MembershipTypeDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vidley.Dtos
+{
+    public class MembershipTypeDTO
+    {
+        public int Id { get; set; }
+
+        [StringLength(256)]
+        public string Name { get; set; }
+
+        
+        public short SignUpFee { get; set; }
+        
+        public byte DiscountRate { get; set; }
+        
+        public byte DurationInMonths { get; set; }
+
+        public static readonly int Unknown = 1;
+        public static readonly int PayAsYouGo = 1;
+    }
+}
